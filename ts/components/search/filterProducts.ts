923f41e68eadcbb2fc8ee00d70acc3f75c18b808
Therese <therese.lybo@gmail.com>
2021-06-01 12:18:30 +0200

    Convert search related files

--- ts/components/search/filterProducts.ts
+++ ts/components/search/filterProducts.ts
@@ -1,10 +1,16 @@
 import { removeFromStorage, searchKey } from "../../utils/storage.js";
 import renderProducts from "../products/renderProducts.js";
+import { Product } from "../../settings/interfaces";
 
-export default function filterProducts(products, searchValue) {
-  const container = document.querySelector(".products-container");
+export default function filterProducts(
+  products: Product[],
+  searchValue: string
+) {
+  const container = document.querySelector(
+    ".products-container"
+  ) as HTMLDivElement;
 
-  const filteredProducts = products.filter((product) => {
+  const filteredProducts = products.filter((product: Product) => {
     const title = product.title.toLowerCase();
     const description = product.description.toLowerCase();
     if (title.includes(searchValue) || description.includes(searchValue)) {
--- ts/components/search/searchProducts.ts
+++ ts/components/search/searchProducts.ts
@@ -2,13 +2,17 @@ import { saveToStorage, searchKey } from "../../utils/storage.js";
 import getProducts from "../../products.js";
 
 export default function searchProducts() {
-  const searchForms = document.querySelectorAll(".product-search-form");
+  const searchForms = document.querySelectorAll(
+    ".product-search-form"
+  ) as NodeListOf<HTMLFormElement>;
 
   searchForms.forEach((form) => {
     form.onsubmit = (e) => {
       e.preventDefault();
-      const searchField = form.querySelector(".product-search");
-      const searchValue = searchField.value.trim().toLowerCase();
+      const searchField = form.querySelector(
+        ".product-search"
+      ) as HTMLInputElement;
+      const searchValue: string = searchField.value.trim().toLowerCase();
 
       saveToStorage(searchKey, searchValue);
       const pathname = location.pathname;
